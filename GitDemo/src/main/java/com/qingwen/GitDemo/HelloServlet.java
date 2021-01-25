64d94b06bb5adeb0d249da8d7624907617aa1a96
zhangqingwen <zhangqingwen@outlook.com>
2021-01-25 20:04:25 +0800

    New Project

--- GitDemo/src/main/java/com/qingwen/GitDemo/HelloServlet.java
+++ GitDemo/src/main/java/com/qingwen/GitDemo/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "sdfdsaffa</h1>");
         out.println("</body></html>");
     }
 
